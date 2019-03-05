476270988ed7c9c8cad747ccad06a6dcf80cc977
Cian O'Ruanaidh <cianor.social@gmail.com>
2019-03-05 12:20:32 -0500

    starting RacePoints html

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
 import axios from 'axios';
+import RacePoints from './RacePoints.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <RacePoints />
       </div>
     );
   }
--- src/RacePoints.js
+++ src/RacePoints.js
@@ -1,2 +1,15 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
+import './RacePoints.css'
+
+class RacePoints extends Component {
+    render() {
+      return (
+        <section className="RacePoints">
+            <h1>Pace Points Section</h1>
+        </section>
+      );
+    }
+  }
+
+export default RacePoints;
