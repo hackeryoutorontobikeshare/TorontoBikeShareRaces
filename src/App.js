761152fcfea5c3ed1828fa4aa6fe28c169e05bad
Linghao Lin <linlinghao@gmail.com>
2019-03-04 19:31:04 -0500

    create start kit for react app, installed firebase, axios. create each components we are coming up with so far

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import firebase from './firebase.js';
+import axios from 'axios';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
