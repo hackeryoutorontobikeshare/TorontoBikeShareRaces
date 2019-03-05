bb739ff0091c91aa79a9cf20acb1ccbfca7a4b93
Maeesha <maeesha.rahman@gmail.com>
2019-03-05 12:19:56 -0500

    started result page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import firebase from './firebase.js';
 import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      Name: [],
+      Desc: [],
+      Race: []
+    }
+  }
+
   render() {
     return (
       <div className="App">
