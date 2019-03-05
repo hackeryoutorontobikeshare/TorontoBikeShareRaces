75b4a60b80bd9a4440bb794ca0f0957222e6ce10
Gus <gusrichardson1@gmail.com>
2019-03-05 15:17:43 -0500

    name and desc with basic styling

--- src/NameDesc.js
+++ src/NameDesc.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import './styles/NameDesc.css';
 import firebase from './firebase.js';
 
 class NameDesc extends Component {
     render() {
         return (
-            <section className="NameDesc">
-                <form>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" />
-                    <label htmlFor="desc">Description</label>
-                    <textarea rows="4" columns="50" id="desc"></textarea>
-                </form>
+            <section className="nameDesc">
+                <h1 className="ndTitle">create race name <span className="noCaps">and</span> description</h1>
+                <div className="ndWrapper">
+                    <form className="ndForm">
+                        <span className="ndSpan">
+                            <label htmlFor="name">Name</label>
+                        </span>
+                        <span className="ndSpan">
+                            <input type="text" id="name" placeholder="Enter name" />
+                        </span>
+                        <span className="ndSpan">
+                            <label htmlFor="desc">Description</label>
+                        </span>
+                        <span className="ndSpan">
+                            <input type="text" id="desc" placeholder="Enter description" />
+                        </span>
+                    </form>
+                </div>
+                <button className="ndButton">Next</button>
             </section>
         );
     }
