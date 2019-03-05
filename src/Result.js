1226bcee1b74497befa2e1bb45bda72c57c54d73
Maeesha <maeesha.rahman@gmail.com>
2019-03-05 16:51:16 -0500

    some minor changes and basic styling for results

--- src/Result.js
+++ src/Result.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
 import './styles/Result.css';
+import raceEnd from './raceEnd.png'
 
 class RaceInfo extends Component {
     
@@ -31,23 +32,25 @@ class RaceInfo extends Component {
                 </nav>
             </header>
                 {
-                    <div className="wrapper">
-                        <h2>Your Race Information</h2>
-                        {
-                            <div className="raceInfo">
+                <div className="wrapper">
+                        <div className="raceInfo">
+                            <div className="container">
+                                <h2>Your Race Information</h2>
                                 <h3>Race Name: {this.props.name}</h3>
                                 <p>Description: {this.props.description}</p>
                                 <div className="racePoints">
-                                    <p className="startP">Starting Point: {this.props.startP}</p>
+                                    <p className="startP"><i class="fas fa-flag-checkered"></i> Starting Point: {this.props.startP}</p>
                                     <p className="checkP">Check Point: {this.props.checkP}</p>
-                                    <p className="endP">Ending Point: {this.props.endP}</p>
+                                    <p className="endP">
+                                    <img src={raceEnd} alt="Race Finish Banner"/>
+                                     Ending Point: {this.props.endP}</p>
                                     <button onClick={this.handleSaveRace} className="save">Save Race</button>
                                 </div>
-                                <p className="laws">Please obey all traffic laws while participating in a race!</p>
+                                <p className="laws"><i class="fas fa-exclamation-triangle"></i> Please obey all traffic laws while participating in a race! <i class="fas fa-exclamation-triangle"></i></p>
                             </div>
-                        }
-                        <button onClick={this.handlePrevRaces} className="prevRaces">Previous Races</button>
-                    </div>
+                        </div>
+                    <button onClick={this.handlePrevRaces} className="prevRaces">Previous Races</button>
+                </div>
                 }
             </section>
         );
