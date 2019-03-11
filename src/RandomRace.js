6ac9fe7f86788be48aabbc7531c48535b3a19e67
Gus <gusrichardson1@gmail.com>
2019-03-11 11:47:23 -0400

    updated randomrace file

--- src/RandomRace.js
+++ src/RandomRace.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import { posix } from 'path';
 
 
@@ -7,9 +8,50 @@ class RandomRace extends Component {
         super();
         this.state = {
             longitude: 0,
-            latitude: 0
+            latitude: 0,
+            hasCoords: false
         }
     }
+
+    componentDidMount() {
+        this.getLocation()
+        // this.getStationCoords()
+    }
+
+    //API call
+    getStationCoords = () => {
+        return axios({
+            method: 'GET',
+            url: 'http://api.citybik.es/v2/networks/toronto',
+            dataResponse: 'json'
+        })
+            .then((response) => {
+                console.log(response.data.network.stations, "this is the response data stations");
+                const stations = response.data.network.stations;
+                const stationCoordsArr = [];
+                const totalDist = [];
+                stations.forEach((station) => {
+                    console.log(this.state.latitude);
+
+                    let latDist = Math.abs(station.latitude - this.state.latitude);
+                    let lngDist = Math.abs(station.longitude - this.state.longitude);
+                    let totDist = latDist + lngDist;
+                    let stn = {
+                        name: station.name,
+                        totalDist: totDist
+                    }
+
+                    totalDist.push(stn)
+                })
+                totalDist.sort(function (a, b) {
+                    return a.totalDist - b.totalDist
+                })
+                console.log(totalDist)
+
+            })
+    }
+
+
 
 
     getLocation = () => {
@@ -20,7 +62,8 @@ class RandomRace extends Component {
                 console.log(lat, long)
                 this.setState({
                     longitude: long,
-                    latitude: lat
+                    latitude: lat,
+                    hasCoords: true
                 }, () => {
                     console.log(this.state, "this is the state")
                 })
@@ -32,8 +75,7 @@ class RandomRace extends Component {
     render() {
         return (
             <div>
-                <button onClick={this.getLocation}>This is the location button</button>
-                <button onClick={this.logInfo}>this is the log button</button>
+                {this.state.hasCoords ? (<button onClick={this.getStationCoords}>This is the location button</button>) : (<div></div>)}
             </div>
         )
     }
