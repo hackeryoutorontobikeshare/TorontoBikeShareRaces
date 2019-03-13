09f79bad5a82af7972203b246a5ad1996abbfca6
Linghao Lin <linlinghao@gmail.com>
2019-03-12 21:21:51 -0400

    Updates

---
+++ precache-manifest.477f4cab2b81d0539014cb845e55a7b3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f7aa3b7d648c4057970",
+    "url": "/static/css/main.439c0bab.chunk.css"
+  },
+  {
+    "revision": "1f7aa3b7d648c4057970",
+    "url": "/static/js/main.1f7aa3b7.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c913b918f8eb61150683",
+    "url": "/static/js/2.c913b918.chunk.js"
+  },
+  {
+    "revision": "19fabbdf71677064ad82e439bc585148",
+    "url": "/static/media/logo.19fabbdf.png"
+  },
+  {
+    "revision": "633e74dc00ed9e4f04353568590b54e5",
+    "url": "/static/media/racePointsImage.633e74dc.jpg"
+  },
+  {
+    "revision": "c6c00cabd57368b27ef9fd57f7c75f61",
+    "url": "/static/media/bixiCity.c6c00cab.jpg"
+  },
+  {
+    "revision": "7c5b089d617bfda95103896931b1b3a3",
+    "url": "/index.html"
+  }
+];
