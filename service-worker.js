c5dd11e5a61b9c1ca8b25d2385a479dfc42d0c19
Linghao Lin <linlinghao@gmail.com>
2019-03-12 21:40:03 -0400

    Revert "Updates"
    This reverts commit 559e23e049d2f596ed1b8840b511cd4126f9a13d.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48be215bb72941c678238b57b47ba44f.js"
+  "/TorontoBikeShareRaces/precache-manifest.4a26c439bc02064efd6ef5f9145fe290.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/TorontoBikeShareRaces/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
